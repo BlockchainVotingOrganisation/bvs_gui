65ecc135c0196ba9dcfc561798b89bde6c63784b
Louis Göttertz <info2017@goettertz.de>
2017-07-17 15:28:55 +0200

    Data between BVS main window and project select dialog

--- bvs_wallet.cpp
+++ bvs_wallet.cpp
@@ -36,8 +36,14 @@ void BVS_Wallet::on_actionWahl_ffnen_triggered()
     ProjekteAuswahl *p = new ProjekteAuswahl;
 
    // connect to p projectliste selected item text in dialog window projectauswahl connect(p,
-//    connect(p, p->on_projectListe_itemSelectionChanged(), this, );
+
+//    connect(p, SIGNAL(p->getProject()), this, ui->label->setText(project));
     p->show();
+
+    if (p->exec()==QDialog::Accepted) {
+        QString project = p->getProject();
+        ui->label->setText(project);
+    }
 
 
 }
