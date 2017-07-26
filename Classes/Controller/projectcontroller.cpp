2fd0452bb5c75e0bbff06070f2d7377b78c8c03f
Louis Göttertz <com@goettertz.de>
2017-07-26 13:20:57 +0200

    Update projectcontroller.cpp

--- Classes/Controller/projectcontroller.cpp
+++ Classes/Controller/projectcontroller.cpp
@@ -11,12 +11,3 @@ QStringList ProjectController::listAction(QStringList args) {
     ProjectRepository *repository;
     return repository->findAllProjects(args);
 }
-
-//Project ProjectController::showAction(QString project) {
-//    ProjectRepository *repository;
-
-////    ballots = repository->GetBallots(project);
-//    return myproject;
-//}
-
-
