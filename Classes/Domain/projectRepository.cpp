9ad5d906043181c340261cff27e317e072a2a34b
Louis Göttertz <com@goettertz.de>
2017-07-26 13:18:53 +0200

    Update projectRepository.cpp

--- Classes/Domain/projectRepository.cpp
+++ Classes/Domain/projectRepository.cpp
@@ -6,8 +6,6 @@
 #include <QJsonObject>
 #include <QDebug>
 
-//using namespace std;
-
 /**
  * @brief ProjectRepository::ProjectRepository
  */
@@ -24,12 +22,10 @@ ProjectRepository::ProjectRepository()
  */
 QStringList ProjectRepository::findAllProjects(QStringList args) {
 
-    QStringList projectList;
-    QString cmd;
-
-    QString blockchain, path;
-    QStringList arguments;
+    QStringList projectList, arguments;
     QProcess process;
+    QByteArray stdOut;
+    QString cmd, blockchain, path;
 
     for(int i = 0; i < args.length(); i++) {
         QStringList argBlockchain= args.at(i).split("=");
@@ -40,54 +36,39 @@ QStringList ProjectRepository::findAllProjects(QStringList args) {
             path = argBlockchain[1];
         }
     }
+
     arguments.append(blockchain);
     arguments.append("liststreams");
 
-    cmd = path + "multichain-cli.exe";
+    cmd = path + "multichain-cli";
 
     process.start(cmd, arguments);
-    if (process.waitForFinished()) {
+    process.waitForFinished();
 
-        QString out =  process.readAllStandardOutput();
-        QByteArray stdOut;
-        QJsonParseError jerror;
+    stdOut.append(process.readAllStandardOutput());
 
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(stdOut);
 
-        /*
-         * Work-Around für Windows...
-         */
-        if (out.contains("method") && out.contains("liststreams")) {
-            QStringList outputChars;
-            outputChars = out.split("\r\n\r\n");
-            qDebug() << "Method?" << outputChars[0];
-            stdOut = outputChars[1].toUtf8().replace("\r\n","");
-        }
-        else
-            stdOut = out.toUtf8().replace("\r\n","");
-
-
-        QJsonDocument jsonDocument = QJsonDocument::fromJson(stdOut, &jerror); // <- funktioniert nicht.
-
-        qDebug() << "jerror:" << jerror.errorString();
-        if (!jsonDocument.isNull())
-        {
-            if (jsonDocument.isArray()) {
-                QJsonArray jsonArray = jsonDocument.array();
-                QJsonObject streams[jsonArray.size()];
-                for (int i = 0; i < jsonArray.size(); i++) {
-                  streams[i] = jsonArray[i].toObject();
-                  projectList.append(streams[i].value("name").toString());
-                }
+    if (!jsonDocument.isNull())
+    {
+        if (jsonDocument.isArray()) {
+            jsonDocument = QJsonDocument::fromJson(stdOut);
+            QJsonArray jsonArray = jsonDocument.array();
+            QJsonObject streams[jsonArray.size()];
+            for (int i = 0; i < jsonArray.size(); i++) {
+              streams[i] = jsonArray[i].toObject();
+              projectList.append(streams[i].value("name").toString());
             }
-            else {
-                qDebug() << "JSON is not an array!";
-            }
+
         }
         else {
-            qDebug() << "JSON is null!";
+            qDebug() << "JSON is not an array!";
         }
-        return projectList;
     }
+    else {
+        qDebug() << "JSON is null!";
+    }
+    return projectList;
 }
 
 /**
@@ -98,10 +79,9 @@ QStringList ProjectRepository::findAllProjects(QStringList args) {
 QStringList ProjectRepository::findAllItems(QStringList args, QString project) {
 
     QStringList itemList, arguments;
-    QByteArray stdOut;
-    QString blockchain, path, out;
     QProcess process;
-    QJsonParseError jerror;
+    QByteArray stdOut;
+    QString blockchain, path;
 
     for(int i = 0; i < args.length(); i++) {
         QString arg = args[i];
@@ -114,13 +94,10 @@ QStringList ProjectRepository::findAllItems(QStringList args, QString project) {
             path = argBlockchain[1];
         }
     }
-
     arguments.append(blockchain);
     arguments.append("subscribe");
-    arguments.append(project);
-    arguments.append("false");
+    process.start(path + "multichain-cli", arguments);
 
-    process.startDetached(path + "multichain-cli.exe", arguments); //project);
     process.waitForFinished();
 
     arguments.clear();
@@ -130,27 +107,13 @@ QStringList ProjectRepository::findAllItems(QStringList args, QString project) {
     arguments.append("false");
     arguments.append("999999");
 
-    process.start(path + "multichain-cli.exe", arguments); //project);
+    process.start(path + "multichain-cli", arguments);
     process.waitForFinished();
-    out =  process.readAllStandardOutput();
 
-    /*
-     * Work-Around für Windows...
-     */
-    if (out.contains("method") && out.contains("liststreamitems")) {
-        QStringList outputChars;
-        outputChars = out.split("\r\n\r\n");
-        qDebug() << "Method?" << outputChars[0];
-        stdOut = outputChars[1].toUtf8().replace("\r\n","");
-    }
-    else
-        stdOut = out.toUtf8().replace("\r\n","");
 
-//    qDebug() << "\nOutput:" << stdOut;
+    stdOut.append(process.readAllStandardOutput());
 
-    QJsonDocument jsonDocument = QJsonDocument::fromJson(stdOut, &jerror);
-
-//    qDebug() << "\nError:" << jerror.errorString();
+    QJsonDocument jsonDocument = QJsonDocument::fromJson(stdOut);
 
     if (!jsonDocument.isNull())
     {
@@ -162,7 +125,6 @@ QStringList ProjectRepository::findAllItems(QStringList args, QString project) {
               streams[i] = jsonArray[i].toObject();
               itemList.append(streams[i].value("key").toString() + ": " + streams[i].value("data").toString());
             }
-        return itemList;
         }
         else {
             qDebug() << "JSON is not an array!";
@@ -171,4 +133,5 @@ QStringList ProjectRepository::findAllItems(QStringList args, QString project) {
     else {
         qDebug() << project << "JSON is null!";
     }
+    return itemList;
 }
